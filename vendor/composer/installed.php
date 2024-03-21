5927e1a7b5b8a6728494d9f2d78c7df10d2b5781
Idris Musa <idrismusa004@gmail.com>
2024-03-21 09:54:13 +0000

    Updated the resume and added a new resume

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '15aea4acf7112b69296d63a61da3b80aa345c4c5',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '15aea4acf7112b69296d63a61da3b80aa345c4c5',
+    ),
+  ),
+);
